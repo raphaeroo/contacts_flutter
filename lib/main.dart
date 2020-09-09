5c00723434090d738549e5650488a239f92a310c
Raphael Freitas <raphael.silva@midway.com.br>
2020-09-09 13:41:16 -0300

    create contact-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/ui/contact_page.dart';
 import 'package:flutter_contacts/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: new HomePage(),
+    home: new ContactPage(),
   ));
 }
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/helpers/contact_helper.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editedContact = new Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? 'Novo Contato'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
